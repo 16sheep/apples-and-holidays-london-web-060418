d942708f94804d38a73a59b31277060045f202f4
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-24 12:26:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |key, values|
     print "#{key.capitalize}:\n"
-    print " #{values.keys}:\n"
+    print " #{values.keys.flatten.capitalize}:\n"
+    print ""
   end
   
   
