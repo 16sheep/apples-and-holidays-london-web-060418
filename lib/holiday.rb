6d6a908400678fd6ade499a57ac03abc361ca2f7
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-24 07:40:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   print holiday_hash
-  holiday_hash[season] = holiday_name
   holiday_hash[season][holiday_name] = supply_array
   print holiday_hash
   holiday_hash
